520678c2ee24bd49c1b5fd82b464d29f97aaf00d
dtssirimanne <dtssirimanne225@gmail.com>
2025-01-27 15:00:09 +0530

    Add the javascript

--- script.js
+++ script.js
@@ -16,17 +16,23 @@ else{
 
 } 
 inputBox.value ="";
+saveData();
 }
 listContainer.addEventListener("click",function(e){
-    if(e.target.targetName === "LI"){
+    if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
-
+        saveData();
     }
 },false);
 
 function saveData(){
     localStorage.setItem("data",listContainer.innerHTML);
 }
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
